aab356c2c7701e33e283f42a5f6f618b165fdf64
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-21 18:17:53 -0500

    Definiendo endpoints de categorias

--- routes/categorias.js
+++ routes/categorias.js
@@ -2,12 +2,40 @@ const { Router } = require('express');
 const {check} = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const hola
+
 
 const router = Router();
+//Obtener todas las categorias - publico
+router.get("/",(req,res)=>{
+    res.json({
+        msg:'get'
+    })
+});
+//Obtener una categoria por id - publico
+router.get("/:id",(req,res)=>{
+    res.json({
+        msg:'get - id'
+    })
+});
+//Crear una nueva categoria - privado - cualquier persona con un token valido
+router.post("/",(req,res)=>{
+    res.json({
+        msg:'post'
+    })
+});
+//Actualizar - privado - cualquiera con token valido
+router.put("/:id",(req,res)=>{
+    res.json({
+        msg:'put'
+    })
+});
+//Borrar una categoria - admin
+router.delete("/:id",(req,res)=>{
+    res.json({
+        msg:'delete'
+    })
+});
 
-router.get('/',(req,res)=>{
-    console.log("TODO OK!");
-})
+
 
 module.exports = router
