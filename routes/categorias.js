cb8c5a3f11e2103ce321df6c679c3c5253301f19
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-21 17:57:03 -0500

    Declaracion de variable

--- routes/categorias.js
+++ routes/categorias.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const {check} = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const hola
 
 const router = Router();
 
