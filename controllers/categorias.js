077e7794b1f90d19c3f723e870c2f4a5e47b5d8a
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-21 19:07:46 -0500

    Creando controlador de crear Categoria

---
+++ controllers/categorias.js
@@ -0,0 +1,42 @@
+const {response} = require('express');
+
+const Categoria= require('../models/categoria')
+
+const crearCategoria = async (req,res = response) => {
+    
+    try {
+        const nombre = req.body.nombre.toUpperCase();
+        const categoriaDB = await Categoria.findOne({nombre});
+        if (categoriaDB) {
+            return res.status(400).json({
+                msg: `La categoria ${categoriaDB.nombre} ya existe!`
+            })
+        }
+        //Generar la data a guardar
+        const data = {
+            nombre,
+            usuario:req.usuario._id
+        }
+    
+        const categoria = new Categoria(data);
+    
+        //guardar DB
+        await categoria.save();
+    
+        return res.status(200).json({
+            msg: `Categoria ${nombre} creada!`,
+            categoria
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            msg: `Hubo un error al crear la Categoria`,
+            error
+        });
+    }
+
+}
+
+module.exports = {
+    crearCategoria
+}
