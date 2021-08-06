87187a03d1218efdc7bba6634f3c4e0e973cb036
Aldo Navarrete <aaldiitoo@gmail.com>
2021-08-05 21:33:43 -0500

    busqueda por campo

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -2,7 +2,7 @@ const {response} = require('express');
 const {ObjectId} = require('mongoose').Types;
 
 
-const {Usuario,Categoria,Producto} = require('../models');
+const {Usuario,Categoria,Producto,Role} = require('../models');
 
 const coleccionesPermitidas = [
     'usuarios',
@@ -35,9 +35,77 @@ const buscarUsuarios = async (termino = '',res = response) =>{
     }
 }
 const buscarProducto = async (termino = '',res = response) =>{
-    res.json({
-        msg: 'buscarProducto:',
-    })
+
+    const filter = {_id:termino,estado:true};
+
+    // Por correo
+    const esMongoId = ObjectId.isValid(termino);
+    if (esMongoId) {
+        const producto = await Producto.findOne(filter).populate("categoria");
+        return res.status(200).json( {
+            msg: 'productoBuscado:',
+            results: producto? [producto]: []
+        })
+    }else{
+       // Por nombre
+       const regex = new RegExp(termino,'i');
+       const productos = await Producto.find({
+           estado:true,
+           $or:[{nombre:regex}]
+        })
+        .populate("categoria");
+       return res.status(productos?200:404).json( {
+           msg: 'productosEncontrados:',
+           results: productos? [productos]: []
+       })
+    }
+}
+const buscarCategoria = async (termino = '',res = response) =>{
+    const filter = {_id:termino,estado:true};
+
+    // Por correo
+    const esMongoId = ObjectId.isValid(termino);
+    if (esMongoId) {
+        const categoria = await Categoria.findOne(filter);
+        return res.status(200).json( {
+            msg: 'categoriaBuscada:',
+            results: categoria? [categoria]: []
+        })
+    }else{
+       // Por nombre
+       const regex = new RegExp(termino,'i');
+       const categorias = await Categoria.find({
+           estado:true,
+           $or:[{nombre:regex},{correo:regex}]
+        });
+       return res.status(categorias?200:404).json( {
+           msg: 'categoriasEncontrados:',
+           results: categorias? [categorias]: []
+       })
+    }
+}
+const buscarRol = async (termino = '',res = response) =>{
+    const filter = {_id:termino};
+
+    // Por correo
+    const esMongoId = ObjectId.isValid(termino);
+    if (esMongoId) {
+        const rol = await Role.findOne(filter);
+        return res.status(200).json( {
+            msg: 'rolBuscado:',
+            results: rol? [rol]: []
+        })
+    }else{
+       // Por nombre
+       const regex = new RegExp(termino,'i');
+       const roles = await Role.find({
+           $or:[{rol:regex}]
+        });
+       return res.status(roles?200:404).json( {
+           msg: 'rolesEncontrados:',
+           results: roles? [roles]: []
+       })
+    }
 }
 
 const buscar = (req,res = response) =>{
@@ -54,13 +122,13 @@ const buscar = (req,res = response) =>{
             buscarUsuarios(termino,res);
             break;
         case 'categorias' :
-            //Linea
+            buscarCategoria(termino,res);
             break;
         case 'productos':
-            buscarProducto(termino,res)
+            buscarProducto(termino,res);
             break;
         case 'roles':
-            //Linea
+            buscarRol(termino,res);
             break;
         default:
             res.status(500).json({
