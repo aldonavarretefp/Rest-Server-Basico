290493b6ff231caa457e13a5449a98d55bc39ef8
Aldo Navarrete <aaldiitoo@gmail.com>
2021-08-09 12:16:06 -0500

    Controlador de actualizar imagen de usuario o producto

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,8 @@
 
 const { response} = require('express');
-const { extname } = require('path');
+
 const { subirArchivo } = require('../helpers/subir-archivo');
+const {Usuario,Producto} = require('../models');
 
 
 const cargarArchivo = async (req,res = response) => {
@@ -27,9 +28,41 @@ const cargarArchivo = async (req,res = response) => {
 
 const actualizarImagen = async  (req,res=response) => {
     const {id,coleccion} = req.params;
-    res.status(200).json({
-        id,coleccion
-    })
+
+    let modelo;
+    const filters = {estado:true,_id:id}
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findOne(filters)
+            if (!modelo){
+                return res.status(404).json({
+                    msg: `El usuario con ID ${id} no existe`
+                })
+            }
+        break;
+        case 'productos':
+            modelo = await Producto.findOne(filters).populate('categoria');
+            if (!modelo){
+                return res.status(404).json({
+                    msg: `El producto con ID ${id} no existe`
+                });
+            }
+        break;
+    
+        default:
+            return res.status(500).json({
+                msg:'Comuniquese con el administrador para este error'
+            });        
+    }
+
+    const nombreImg = await subirArchivo(req.files,undefined,coleccion);
+    modelo.img = nombreImg;
+
+
+    await modelo.save();
+
+    res.status(200).json(modelo)
 }
 
 module.exports = {
