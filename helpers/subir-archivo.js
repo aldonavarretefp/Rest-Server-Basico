2c57d2749d0e6e7b73b118ece9e5e7eee2a205d2
Aldo Navarrete <aaldiitoo@gmail.com>
2021-08-10 17:05:09 -0500

    Arreglando destructuración del archivo de la request

--- helpers/subir-archivo.js
+++ helpers/subir-archivo.js
@@ -1,27 +1,27 @@
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 
-const subirArchivo = ({archivo},extensionesValidas = ['png','jpg','jpeg','gif'],carpeta='') => {
-    return new Promise((resolve,reject)=> {
-        const separarPorPunto = archivo.name.split('.') ; // ['nombredelarchivo','extension' ]
-        const extension = separarPorPunto[separarPorPunto.length-1];
-        if(!extensionesValidas.includes(extension)){
-            return reject(`Extensión ${extension} no permitida - ${extensionesValidas}`)
-        }
-        const nombreTemp = uuidv4() + "." + extension;
-    
-        const uploadPath = path.join(__dirname,'../uploads/',carpeta,nombreTemp);
-    
-        archivo.mv(uploadPath, function(err) {
-            if (err) {
-                return reject(err)
+const subirArchivo = (files,extensionesValidas = ['png','jpg','jpeg','gif'],carpeta='') => {
+        return new Promise((resolve,reject)=> {
+            const {archivo} = files;
+            const separarPorPunto = archivo.name.split('.') ; // ['nombredelarchivo','extension' ]
+            const extension = separarPorPunto[separarPorPunto.length-1];
+            if(!extensionesValidas.includes(extension)){
+                return reject(`Extensión ${extension} no permitida - ${extensionesValidas}`)
             }
-            resolve(nombreTemp);
-
+            const nombreTemp = uuidv4() + "." + extension;
+        
+            const uploadPath = path.join(__dirname,'../uploads/',carpeta,nombreTemp);
+        
+            archivo.mv(uploadPath, function(err) {
+                if (err) {
+                    reject(err)
+                }
+               resolve(nombreTemp);
+    
+            });
+    
         });
-
-    })
-
 }
 module.exports = {
     subirArchivo
