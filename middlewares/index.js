2e6213c4b775fcf6286f0ccce94e2f4789a320d8
Aldo Navarrete <aaldiitoo@gmail.com>
2021-08-10 17:04:43 -0500

    Middleware e index para subir Archivo

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,11 @@
 const validarCampos  = require('../middlewares/validar-campos');
 const  validarJWT  = require('../middlewares/validar-jwt');
 const  {esAdministrador, tieneRole}  = require('../middlewares/validar-roles');
-
+const validarArchivoSubir = require('../middlewares/validar-archivo');
 module.exports = {
     ...validarCampos,
     ...validarJWT,
     esAdministrador,
-    tieneRole
-
+    tieneRole,
+    ...validarArchivoSubir
 }
