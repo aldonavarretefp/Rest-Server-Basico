9acc29d3334daeff3cdb9028b5b99f81987ce51a
Aldo Navarrete <aaldiitoo@gmail.com>
2021-08-10 17:03:55 -0500

    Middleware para validar si hay archivo en la petición

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,15 +2,18 @@ const {Router} = require('express');
 const {body,check} = require('express-validator');
 const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers/db-validators');
+const { validarArchivoSubir ,validarCampos } = require('../middlewares');
 
-const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 
-router.post('/',cargarArchivo);
+router.post('/',[
+    validarArchivoSubir
+], cargarArchivo);
 
 router.put('/:coleccion/:id',[
+    validarArchivoSubir,
     check('id','MONGOID no valido').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c,['usuarios','productos'])),
     validarCampos
